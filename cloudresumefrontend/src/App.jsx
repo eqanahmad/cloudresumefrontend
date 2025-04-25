c5b4afcfdb0eb4ec6b28f8a06061d45df7a54ab1
eqanahmad <eqanahmad@gmail.com>
2025-04-25 11:14:32 -0400

    init commit!

---
+++ cloudresumefrontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <div className="min-h-screen bg-background p-4 md:p-8">
+      <main className="container mx-auto max-w-3xl space-y-24">
+      <div className="dialog-box">
+          <div className="dialog-name">Cloud Resume Challenge, sort of...</div>
+          <div className="dialog-avatar" />
+          <div className="dialog-content">
+            <p className="mb-6">
+              Welcome to my Cloud Resume Challenge build log! Instead of hosting my resume, 
+              I’m using this space to document the architecture and components of the project itself
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+    </>
+  )
+}
+
+export default App
